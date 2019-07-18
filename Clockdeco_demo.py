88f8840a680e8c274ea2f9f944c3445858ad7222
deepudoit <pradeepdoit@gmail.com>
2019-07-18 22:23:31 +0530

    Updated decoratos with functools

--- Clockdeco_demo.py
+++ Clockdeco_demo.py
@@ -1,3 +1,7 @@
+import time
+from Clockdeco import clock
+
+
 @clock
 def snooze(sec):
     time.sleep(sec)
@@ -6,3 +10,10 @@ def snooze(sec):
 @clock
 def factorial(n):
     return 1 if n < 2 else n * factorial(n - 1)
+
+
+if __name__ == "__main__":
+    print('*' * 40, 'calling snooze(.123)')
+    snooze(.123)
+    print('*' * 40, 'calling factorial(6)')
+    print('6!=', factorial(6))
